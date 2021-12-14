a4121b23730c48699fe43a8f438a65da42bd5df1
yazid113 <92461753+yazid113@users.noreply.github.com>
2021-12-13 22:25:09 -0500

    Final Commit

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,7 @@
+//Variable creation for each of the selected areas of the form
+const $form = $('form')
 const $nameField = $('#name')
+const $emailField = $('#email')
 const $jobRole = $('#title')
 const $otherRole = $('#other-job-role').hide()
 const $shirtDesign = $('#design')
@@ -9,21 +12,23 @@ const $payFormat = $('#payment')
 const $payPal = $('#paypal').hide()
 const $bitCoin = $('#bitcoin').hide()
 const $creditCard = $('#credit-card')
+const $cardNumber = $('#cc-num')
+const $zipCode = $("#zip")
+const $cvv = $('#cvv')
+const $checkbox = $('input[type="checkbox"]')
+//Initalizatiom variables
 let total = 0;
+//The color form is disbled until the design is selected
 $colorChildren.prop('disabled', true)
+//Onload display credit card option
 $payFormat.children().eq(1).attr('selected','selected')
-
-
-// Focus input 'Name' on load
-$(function() {
-    $($nameField).focus();
-  });
+//onload mouse is at the name field
+$nameField.focus();
 
 // Event listener to display the other role  box
 $($jobRole).change(function(e) {
     if (e.target.value === 'other') {
         $otherRole.show()
-        console.log(e.target.value)
     }
     else{
         $otherRole.hide()
@@ -35,8 +40,6 @@ $($shirtDesign).change(function(e) {
     $colorChildren.prop('disabled', false)
     for (let i = 0; i < $colorChildren.length; i++) {
        const $dataTheme = $colorChildren.eq(i).attr('data-theme')
-       console.log($dataTheme)
-       console.log(e.target.value)
      if (e.target.value === $dataTheme) {
         $colorChildren.eq(i).show()
         $colorChildren.eq(i).attr('selected','selected')
@@ -48,28 +51,28 @@ $($shirtDesign).change(function(e) {
     }
   });
 
-// Activities selected function
+//Event listener for the activities selection process verification and the
+//total sum of the cost of each
 $($regisActivities).change(function (e) {
     
     const dataCost = parseInt(e.target.getAttribute('data-cost'))
-   // console.log(e.target)
         if(e.target.checked){
             total = total + dataCost
             $costActivities.html(`Total: $${total}`)
-            return $costActivities
+            return total
            
         }
         else{
             total = total-dataCost;
             $costActivities.html(`Total: $${total}`)
-            return $costActivities
+            return total
         }
        
     
 });
-// Payment information function
+//Event listener for the the payment selection method
+//Hides and show on the site depending on the users selection
 $($payFormat).change(function(e) {
-   console.log(e.target.value)
     if (e.target.value === 'bitcoin') {
         $bitCoin.show()
         $payPal.hide()
@@ -87,3 +90,82 @@ $($payFormat).change(function(e) {
     }
   });
 
+//Validatio of each obligatory field need to submit the form
+//Error or valid disply on the field depeding on each verification
+$($form).submit(function (e) {
+    const $nameValue = $nameField.val()
+    const regexName = /^(?!\s*$)[-a-zA-Z0-9_:,.' ']{1,100}$/i
+    const $emailValue = $emailField.val()
+    const regexEmail = /^[^@]+@[^@.]+\.[a-z]+$/i
+    const $cardValue = $cardNumber.val()
+    const regexCardNumb = /^[0-9]{13,16}$/
+    const regexZipCode = /^[0-9]{5}$/
+    const regexCvv = /^[0-9]{3}$/
+    if (!regexName.test($nameValue)) {
+        $nameField.parent().addClass('not-valid')
+        $nameField.parent().removeClass('valid')
+        console.log($nameField.parent())
+        e.preventDefault();
+    }
+    else{
+        $nameField.parent().removeClass('not-valid')
+        $nameField.parent().addClass('valid')
+    }
+    if (!regexEmail.test($emailValue)) {
+        $emailField.parent().addClass('not-valid')
+        $emailField.parent().removeClass('valid')
+        e.preventDefault();
+    }
+    else{
+        $emailField.parent().removeClass('not-valid')
+        $emailField.parent().addClass('valid')
+    }
+    if(total === 0){
+        $regisActivities.addClass('not-valid')
+        $regisActivities.removeClass('valid')
+        e.preventDefault()
+    }
+    else{
+        $regisActivities.removeClass('not-valid')
+        $regisActivities.addClass('valid')
+    }
+    if ($payFormat.val() === 'credit-card') {
+            if (!regexCardNumb.test($cardValue)) {
+                $cardNumber.parent().addClass('not-valid')
+                $cardNumber.parent().removeClass('valid')
+                e.preventDefault()
+            }
+            else{
+                $cardNumber.parent().removeClass('not-valid')
+                $cardNumber.parent().addClass('valid') 
+            }
+            if (!regexZipCode.test($zipCode.val())) {
+                $zipCode.parent().addClass('not-valid')
+                $zipCode.parent().removeClass('valid')
+                e.preventDefault()
+            }
+            else{
+                $zipCode.parent().removeClass('not-valid')
+                $zipCode.parent().addClass('valid') 
+            }
+            if (!regexCvv.test($cvv.val())) {
+                $cvv.parent().addClass('not-valid')
+                $cvv.parent().removeClass('valid')
+                e.preventDefault()
+            }
+            else{
+                $cvv.parent().removeClass('not-valid')
+                $cvv.parent().addClass('valid') 
+            }
+    }
+});
+//Accessibilty-focus or blur the activities section on selection
+for (let i = 0; i < $checkbox.length; i++) {
+    $checkbox.eq(i).focus(function(){
+     $checkbox.parent().addClass('focus');  
+    })
+    $checkbox.eq(i).blur(function(){
+        $checkbox.parent().removeClass('focus');  
+       })
+    
+}
